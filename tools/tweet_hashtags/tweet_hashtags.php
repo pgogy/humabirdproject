c80b49d649d5cebfaaa235bf878ca5afbd6256c5
humabirdproject <humabird@humabirdproject.org>
2013-11-03 22:44:26 +0000

    New features

--- tools/tweet_hashtags/tweet_hashtags.php
+++ tools/tweet_hashtags/tweet_hashtags.php
@@ -59,6 +59,12 @@
 								<li>
 									<a href='?tool=tweet_hashtags&action=hashtags'>" . $this->language->translate("tools/tweet_hashtags", "Display hashtags") . "</a>
 								</li>
+								<li>
+									<a href='?tool=tweet_hashtags&action=hashtags_used'>" . $this->language->translate("tools/tweet_hashtags", "Display hashtags (Pie Chart)") . "</a>
+								</li>
+								<li>
+									<a href='?tool=tweet_hashtags&action=hashtags_lowercase'>" . $this->language->translate("tools/tweet_hashtags", "Display hashtags (all lower case)") . "</a>
+								</li>
 						   </ul>";
 						   
 			}else{
@@ -79,7 +85,100 @@
 				
 		}
 		
-
+		private function hashtags_lowercase(){
+		
+			if(count($_POST)!==0){
+			
+				require_once("core/file_handling/file_handling.php");
+				$file_process = new file_handling();
+				
+				if($file_process->file_exists_check("data/twitter_harvest/files/aggregate/" . $_POST['tweetfile'])){
+				
+					$data = unserialize($file_process->file_get_all("data/twitter_harvest/files/aggregate/" . $_POST['tweetfile']));
+				
+				}else{
+				
+					$data = unserialize($file_process->file_get_all("data/twitter_harvest/files/usertweets/" . $_POST['tweetfile']));
+				
+				}
+				
+				$output = "<h2>" . $this->language->translate("tools/tweet_hashtags", "Tweet Display for file ") . " : " . $_POST['tweetfile'] . "</h2>";
+				
+				$hashtags = array();
+				
+				foreach($data as $tweet){
+				
+					if(isset($tweet->entities->hashtags)){
+				
+						foreach($tweet->entities->hashtags as $key => $data){
+						
+							if(isset($hashtag[strtolower($data->text)])){
+							
+								$hashtag[strtolower($data->text)]++;
+								
+							}else{
+							
+								$hashtag[strtolower($data->text)]=1;
+								
+							}
+						
+						}
+						
+					}
+					
+				}
+				
+				arsort($hashtag);
+				
+				
+				$output = "<h3>" . $this->language->translate("tools/tweet_hashtags", "Hashtags") . "</h3>";
+				
+				foreach($hashtag as $hash => $count){
+				
+					$output .= "<p>" . $hash . " : " . $count . "</p>";
+				
+				}
+				
+				return $output . "<p><a href='?tool=tweet_hashtags'>" . $this->language->translate("tools/tweet_hashtags", "Return to Tweet Display") . "</a></p>";
+				
+			}else{
+			
+				require_once("core/file_handling/file_handling.php");
+				$file_process = new file_handling();
+				$tweets = array_merge($file_process->read_folder("data/twitter_harvest/files/aggregate/"),$file_process->read_folder("data/twitter_harvest/files/usertweets/"));
+				
+				arsort($tweets);
+				
+				if(count($tweets)!==0){
+				
+					$output = "<h2>" . $this->language->translate("tools/tweet_hashtags", "Display a Tweet file") . "</h2>
+							   <p>" . $this->language->translate("tools/tweet_hashtags", "Choose a Tweet file to start from") . "</p>
+							   <form action='' method='POST'>
+									<select name='tweetfile'>
+										<option>" . $this->language->translate("tools/tweet_hashtags", "Select a file") . "</output>";
+								
+					while($plain = array_pop($tweets)){
+					
+						$output .= "<option value='" . $plain . "'>" . $plain . "</output>";
+					
+					}
+	
+					$output .=	"</select><br />
+									<input type='submit' value='" . $this->language->translate("tools/tweet_hashtags", "display") . "' />
+							   </form>";
+					
+					return $output;
+		
+				}else{
+				
+					$output = "<p>" . $this->language->translate("tools/tweet_hashtags", "No Tweet files exist, please create one for using this tool.") . "</p>";				
+				
+				}
+			
+			}
+		
+		}
+		
 		private function hashtags(){
 		
 			if(count($_POST)!==0){
@@ -173,5 +272,133 @@
 			}
 		
 		}
+	
+		private function hashtags_used(){
 		
+			if(count($_POST)!==0){
+			
+				require_once("core/file_handling/file_handling.php");
+				$file_process = new file_handling();
+				
+				if($file_process->file_exists_check("data/twitter_harvest/files/aggregate/" . $_POST['tweetfile'])){
+				
+					$data = unserialize($file_process->file_get_all("data/twitter_harvest/files/aggregate/" . $_POST['tweetfile']));
+				
+				}else{
+				
+					$data = unserialize($file_process->file_get_all("data/twitter_harvest/files/usertweets/" . $_POST['tweetfile']));
+				
+				}
+				
+				$output = "<h2>" . $this->language->translate("tools/tweet_connect", "Tweet Display for file ") . " : " . $_POST['tweetfile'] . "</h2>";
+				
+				$twitter_hashtags = array();
+				
+				foreach($data as $tweet){
+				
+					if(isset($tweet->entities)){
+				
+						if(count($tweet->entities->hashtags)!=0){
+					
+							foreach($tweet->entities->hashtags as $key => $inner_data){
+							
+								$hashtag = strtolower($inner_data->text);
+								
+								if(!isset($twitter_hashtags[$hashtag])){
+								
+									$twitter_hashtags[$hashtag]=0;
+									
+								}
+								
+								$twitter_hashtags[$hashtag]++;
+							
+							}
+							
+						}
+					
+					}	
+					
+				}
+				
+				arsort($twitter_hashtags);
+				
+				$degree_ratio = 360 / array_sum($twitter_hashtags);
+				
+				$im = imagecreatetruecolor(800,625 + count($twitter_hashtags)*40);
+				$white = imagecolorallocate($im, 255,255,255);
+				$last_angle = 0;
+				$speakers_y = 650;
+				
+				$other_colour = imagecolorallocate($im, 255,255,255);
+				$other_angle = 0;
+				
+				foreach($twitter_hashtags as $key => $data){
+				
+					if(($data*$degree_ratio)>1){
+					
+						$colour = imagecolorallocate($im, rand(10,240),rand(10,240),rand(10,240));
+						imagefilledarc ($im , 400 , 400 , 450 , 450 , $last_angle , $last_angle+($data*$degree_ratio) , $colour, IMG_ARC_PIE );
+						imagefilledrectangle($im, 170, $speakers_y-15 , 190, $speakers_y , $colour );
+						imagettftext ( $im , 15.0 , 0 , 200 , $speakers_y , $white , "core/misc/fonts/arial.ttf" , $key . " "  . $data);
+						$speakers_y += 18;
+						
+					}else{
+					
+						$colour = imagecolorallocate($im, rand(10,240),rand(10,240),rand(10,240));
+						imagefilledrectangle($im, 170, $speakers_y-15 , 190, $speakers_y , $colour );
+						imagettftext ( $im , 15.0 , 0 , 200 , $speakers_y , $white , "core/misc/fonts/arial.ttf" , $key . " "  . $data);
+						$speakers_y += 18;
+					
+					}
+
+					$last_angle += (integer)$data*$degree_ratio;
+					
+				}	
+
+				$file_process->file_image_create("data/twitter_harvest/files/connections/" . str_replace("%23","", str_replace(".","",$_POST['tweetfile'])) . ".jpg", "jpeg", $im);
+				
+				$output = "<h2>" . $this->language->translate("tools/tweet_connect", "Tweet Connections Display") . "</h2>";
+					
+				$output .= "<img src='data/twitter_harvest/files/connections/" . str_replace("%23","", str_replace(".","",$_POST['tweetfile'])) . ".jpg' />";
+
+				return $output . "<p><a href='?tool=tweet_connect'>" . $this->language->translate("tools/tweet_connect", "Return to Tweet Connections Display") . "</a></p>";
+				
+			}else{
+			
+				require_once("core/file_handling/file_handling.php");
+				$file_process = new file_handling();
+				$tweets = array_merge($file_process->read_folder("data/twitter_harvest/files/aggregate/"),$file_process->read_folder("data/twitter_harvest/files/usertweets/"));
+				
+				arsort($tweets);
+				
+				if(count($tweets)!==0){
+				
+					$output = "<h2>" . $this->language->translate("tools/tweet_connect", "Display a Tweet file") . "</h2>
+							   <p>" . $this->language->translate("tools/tweet_connect", "Choose a Tweet file to start from") . "</p>
+							   <form action='' method='POST'>
+									<select name='tweetfile'>
+										<option>" . $this->language->translate("tools/tweet_connect", "Select a file") . "</output>";
+								
+					while($plain = array_pop($tweets)){
+					
+						$output .= "<option value='" . $plain . "'>" . $plain . "</output>";
+					
+					}
+	
+					$output .=	"</select><br />
+									<input type='submit' value='" . $this->language->translate("tools/tweet_connect", "display") . "' />
+							   </form>";
+					
+					return $output;
+		
+				}else{
+				
+					$output = "<p>" . $this->language->translate("tools/tweet_connect", "No Tweet files exist, please create one for using this tool.") . "</p>";				
+				
+				}
+			
+			}
+		
+		}
+	
 	}
--- tools/tweet_time_plot/tweet_time_plot.php
+++ tools/tweet_time_plot/tweet_time_plot.php
@@ -63,6 +63,9 @@
 									<a href='?tool=tweet_time_plot&action=tweet_time_plot_words'>" . $this->language->translate("tools/tweet_time_plot", "Display Tweet time plot (with words)") . "</a>
 								</li>
 								<li>
+									<a href='?tool=tweet_time_plot&action=tweet_time_plot_hashtags'>" . $this->language->translate("tools/tweet_time_plot", "Display Tweet time plot (hashtags only)") . "</a>
+								</li>
+								<li>
 									<a href='?tool=tweet_time_plot&action=tweet_time_plot_words_exclude'>" . $this->language->translate("tools/tweet_time_plot", "Display Tweet time plot (with words + exclusions)") . "</a>
 								</li>
 						   </ul>";
@@ -459,7 +462,177 @@
 			}
 		
 		}
+		
+		private function tweet_time_plot_hashtags(){
+		
+			if(count($_POST)!==0){
 			
+				require_once("core/file_handling/file_handling.php");
+				$file_process = new file_handling();
+				
+				if($file_process->file_exists_check("data/twitter_harvest/files/aggregate/" . $_POST['tweetfile'])){
+				
+					$data = unserialize($file_process->file_get_all("data/twitter_harvest/files/aggregate/" . $_POST['tweetfile']));
+				
+				}else{
+				
+					$data = unserialize($file_process->file_get_all("data/twitter_harvest/files/usertweets/" . $_POST['tweetfile']));
+				
+				}
+				
+				$response = $this->map_tweets_time($data);
+				
+				$windows = $response[0];
+				$max = $response[1];
+				$width = $response[2];
+				$first = $response[3];
+				$last = $response[4];
+				
+				$width_val = round($width/$_POST['time_period']);
+				
+				$im = imagecreatetruecolor(($width_val*20) + 200, 800);
+				$white = imagecolorallocate($im, 255,255,255);
+				
+				imageline($im, 100, 620, 100, 25, $white);
+				imageline($im, 100, 620, ($width_val*20) + 150, 620, $white);
+				
+				$x_pos = 125;
+				$y_pos = 620;
+				
+				for($x=0; $x<=($last-$first); $x+=$_POST['time_period']){
+				
+					$cloud = array();
+				
+					if(isset($windows[$x])){
+					
+						$words = $windows[$x];
+						
+						foreach($words as $index => $tweets){
+						
+							foreach($tweets as $tweet){
+							
+								$word_list = explode(" ", $tweet->text);
+								
+								foreach($word_list as $word){
+								
+									$word = strtolower(trim($word));
+								
+									if(substr($word,0,1)=="#"){
+								
+										if(isset($cloud[$word])){
+										
+											$cloud[$word]++;
+										
+										}else{
+										
+											$cloud[$word]=1;
+										
+										}
+										
+									}
+								
+								}
+							
+							}
+						
+						}
+				
+						$height = ((count($windows[$x])/$max)*600);
+						
+					}else{
+					
+						$height = 0;
+					
+					}
+					
+					imageline($im, $x_pos, $y_pos, $x_pos, $y_pos - $height, $white);
+					imagettftext( $im , 8.0 , 270, $x_pos, 625, $white , "core/misc/fonts/arial.ttf" , date("H:i:s jS M Y", $first+($x)));
+					
+					$small_cloud = array_slice($cloud,0,5);
+					
+					$string = "";
+					
+					foreach($small_cloud as $word => $value){
+					
+						$string .= $word . " (" . $value . ") ";
+					
+					}
+					
+					imagettftext( $im , 8.0 , 90, $x_pos+13, 610, $white , "core/misc/fonts/arial.ttf" , $string);
+					
+					$x_pos+=20;
+					
+				}
+				
+				imagettftext ( $im , 10.0 , 0, 80 , 620, $white , "core/misc/fonts/arial.ttf" , 0);
+				
+				$y_pos_orig = 620 - (600/5);
+				
+				$height = 600 / 5;
+				
+				for($x=1;$x<=5;$x++){
+				
+					imagettftext( $im , 10.0 , 0, 80 , $y_pos_orig, $white , "core/misc/fonts/arial.ttf" ,round(($max/6)*$x,0));
+					
+					$y_pos_orig -= $height;
+				
+				}
+				
+				imagettftext( $im , 8.0 , 0, 125, 760, $white , "core/misc/fonts/arial.ttf" , date("H:i:s jS M Y", $first));
+				
+				imagettftext( $im , 8.0 , 0, 125, 775, $white , "core/misc/fonts/arial.ttf" , date("H:i:s jS M Y", $last));
+				
+				$file_process->file_image_create("data/twitter_harvest/files/time_plot/" . str_replace("%23","", str_replace(".","",$_POST['tweetfile'])) . "_time_plot_words.jpg", "jpeg", $im);
+				
+				$output = "<h2>" . $this->language->translate("tools/tweet_time_plot", "Tweet Time Plot") . "</h2>";
+					
+				$output .= "<img src='data/twitter_harvest/files/time_plot/" . str_replace("%23","", str_replace(".","",$_POST['tweetfile'])) . "_time_plot_words.jpg' />";
+
+				return $output . "<p><a href='?tool=tweet_time_plot'>" . $this->language->translate("tools/tweet_time_plot", "Return to Tweet Time Map") . "</a></p>";
+				
+			}else{
+			
+				require_once("core/file_handling/file_handling.php");
+				$file_process = new file_handling();
+				$tweets = array_merge($file_process->read_folder("data/twitter_harvest/files/aggregate/"),$file_process->read_folder("data/twitter_harvest/files/usertweets/"));
+				
+				arsort($tweets);
+				
+				if(count($tweets)!==0){
+				
+					$output = "<h2>" . $this->language->translate("tools/tweet_time_plot", "Display a Tweet file") . "</h2>
+							   <p>" . $this->language->translate("tools/tweet_time_plot", "Choose a Tweet file to start from") . "</p>
+							   <form action='' method='POST'>
+									<select name='tweetfile'>
+										<option>" . $this->language->translate("tools/tweet_time_plot", "Select a file") . "</output>";
+								
+					while($plain = array_pop($tweets)){
+					
+						$output .= "<option value='" . $plain . "'>" . $plain . "</output>";
+					
+					}
+	
+					$output .=	"</select><br />";
+					
+					$output .= "<label>" . $this->language->translate("data/twitter_time_plot", "Time period") . "</label>";
+	
+					$output .= "<input type='text' name='time_period' />
+								 <input type='submit' value='" . $this->language->translate("tools/tweet_time_plot", "display") . "' />
+							   </form>";
+					
+					return $output;
+		
+				}else{
+				
+					$output = "<p>" . $this->language->translate("tools/tweet_time_plot", "No Tweet files exist, please create one for using this tool.") . "</p>";				
+				
+				}
+			
+			}
+		
+		}
+		
+	
 		private function tweet_time_plot_words_exclude(){
 		
 			if(count($_POST)!==0){
